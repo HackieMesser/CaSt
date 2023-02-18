7e2b4b183fd077a45549ac44a5444c627da4735b
HackieMesser <41595086+HackieMesser@users.noreply.github.com>
2023-02-18 23:12:30 +0100

    Update sqlsolutions.sql

--- sqlsolutions.sql
+++ sqlsolutions.sql
@@ -1,1 +1,4 @@
 
+/*select count(distinct Order_ID) from `case.items` where Article_Group = 'Coffee' ; */
+
+select  sum(Quantity_ordered *  Number_of_capsules / One_or_Two_Cup_Product )/count(distinct Order_ID)  from `case.items` where Article_Group = 'Coffee' 
